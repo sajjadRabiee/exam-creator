f33ebfd7745f8bb9dcb94985a2dd4563dd6ef302
Sajjad <Rabiee.614@gmail.com>
2020-12-16 16:06:50 +0330

    build getter and setter of exam entity

--- src/main/java/com/examcreator/finalproject/entities/classEntities/Exam.java
+++ src/main/java/com/examcreator/finalproject/entities/classEntities/Exam.java
@@ -1,4 +1,81 @@
 package com.examcreator.finalproject.entities.classEntities;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Component
+@Scope("prototype")
+@Entity
 public class Exam {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private LocalDate date;
+
+    @Column
+    private LocalTime startTime;
+
+    @Column
+    private LocalTime endTime;
+
+    @ManyToOne
+    @JoinColumn(foreignKey = "fk_course")
+    private Course course;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
 }
