105ac83cf4c1e01b37e94111e707de3d685fb17d
Sajjad <Rabiee.614@gmail.com>
2021-01-13 14:52:29 +0330

    add method of this controller

--- src/main/java/com/examcreator/finalproject/controller/TeacherController.java
+++ src/main/java/com/examcreator/finalproject/controller/TeacherController.java
@@ -1,8 +1,10 @@
 package com.examcreator.finalproject.controller;
 
-import com.examcreator.finalproject.entities.classEntities.Course;
-import com.examcreator.finalproject.service.CourseService;
-import com.examcreator.finalproject.service.TeacherService;
+import com.examcreator.finalproject.entities.DTOEntities.*;
+import com.examcreator.finalproject.entities.classEntities.OtherObjects.*;
+import com.examcreator.finalproject.entities.enumEntities.TypeOfExam;
+import com.examcreator.finalproject.service.*;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,23 +12,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
 import java.util.List;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping(value = "/teacher")
 public class TeacherController {
+    private final ApplicationContext context;
     private final TeacherService teacherService;
     private final CourseService courseService;
+    private final ExamService examService;
+    private final QuestionService questionService;
+    private final OptionalQuestionService optionalQuestionService;
+    private final TextQuestionService textQuestionService;
 
-    public TeacherController(TeacherService teacherService, CourseService courseService) {
+    public TeacherController(ApplicationContext context, TeacherService teacherService, CourseService courseService, ExamService examService, QuestionService questionService, OptionalQuestionService optionalQuestionService, TextQuestionService textQuestionService) {
+        this.context = context;
         this.teacherService = teacherService;
         this.courseService = courseService;
+        this.examService = examService;
+        this.questionService = questionService;
+        this.optionalQuestionService = optionalQuestionService;
+        this.textQuestionService = textQuestionService;
     }
 
     @GetMapping("/dashboard")
-    public String dashboard(Principal principal, Model model){
+    public String dashboard(Principal principal, Model model) {
         String username = principal.getName();
         List<Course> coursesOfTeacher = courseService.findCoursesOfTeacher(username);
-        model.addAttribute("courses",coursesOfTeacher);
+        model.addAttribute("courses", coursesOfTeacher);
         return "User/Teacher/dashboard";
     }
 }
