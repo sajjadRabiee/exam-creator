4e6018618e67dbc2ee15cfdf766aeaa503533e6f
Sajjad <Rabiee.614@gmail.com>
2021-01-13 14:47:21 +0330

    add new entity with getter and setter and edit some import file location

--- src/main/java/com/examcreator/finalproject/entities/classEntities/OtherObjects/OptionalQuestion.java
+++ src/main/java/com/examcreator/finalproject/entities/classEntities/OtherObjects/OptionalQuestion.java
@@ -3,12 +3,38 @@ package com.examcreator.finalproject.entities.classEntities.OtherObjects;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 @Scope("prototype")
 @Entity
 @DiscriminatorValue(value = "optional_question")
 public class OptionalQuestion extends Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ElementCollection
+    private List<String> options = new ArrayList<>();
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<String> getOptions() {
+        return options;
+    }
+
+    public void setOptions(List<String> options) {
+        this.options = options;
+    }
 }
