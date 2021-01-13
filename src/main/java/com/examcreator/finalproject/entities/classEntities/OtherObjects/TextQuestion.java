8f322468cc416c9864f75f00cf283be61ea75224
Sajjad <Rabiee.614@gmail.com>
2021-01-13 14:49:22 +0330

    add id to this entity

--- src/main/java/com/examcreator/finalproject/entities/classEntities/OtherObjects/TextQuestion.java
+++ src/main/java/com/examcreator/finalproject/entities/classEntities/OtherObjects/TextQuestion.java
@@ -3,12 +3,25 @@ package com.examcreator.finalproject.entities.classEntities.OtherObjects;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Component
 @Scope("prototype")
 @Entity
 @DiscriminatorValue(value = "text_question")
 public class TextQuestion extends Question{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
