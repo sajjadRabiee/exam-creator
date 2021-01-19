631a1563b920a2c26fc8b86b662703e0f4fc9490
Sajjad <Rabiee.614@gmail.com>
2021-01-19 14:33:49 +0330

    complete courese controller

--- src/main/java/com/examcreator/finalproject/controller/CourseController.java
+++ src/main/java/com/examcreator/finalproject/controller/CourseController.java
@@ -3,9 +3,9 @@ package com.examcreator.finalproject.controller;
 import com.examcreator.finalproject.entities.DTOEntities.CourseDTO;
 import com.examcreator.finalproject.entities.DTOEntities.StudentDTO;
 import com.examcreator.finalproject.entities.DTOEntities.TeacherDTO;
-import com.examcreator.finalproject.entities.classEntities.Course;
-import com.examcreator.finalproject.entities.classEntities.Student;
-import com.examcreator.finalproject.entities.classEntities.Teacher;
+import com.examcreator.finalproject.entities.classEntities.OtherObjects.Course;
+import com.examcreator.finalproject.entities.classEntities.Users.Student;
+import com.examcreator.finalproject.entities.classEntities.Users.Teacher;
 import com.examcreator.finalproject.service.CourseService;
 import com.examcreator.finalproject.service.StudentService;
 import com.examcreator.finalproject.service.TeacherService;
@@ -14,8 +14,11 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.SQLException;
+import java.time.format.DateTimeParseException;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -37,70 +40,76 @@ public class CourseController {
 
     @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/add-course")
-    public String showAddCoursePage(Model model){
-        model.addAttribute("course",context.getBean(CourseDTO.class));
-        model.addAttribute("courses",courseService.findAll());
+    public String showAddCoursePage(Model model) {
+        model.addAttribute("course", context.getBean(CourseDTO.class));
+        model.addAttribute("courses", courseService.findAll());
         return "User/Admin/add-course";
     }
 
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/add-course")
-    public String addCourse(@ModelAttribute CourseDTO course, BindingResult result ){
-        courseService.saveCourse(course);
-        return "redirect:/course/add-course";
+    public String addCourse(@ModelAttribute CourseDTO course, BindingResult result) {
+        try {
+            courseService.saveCourse(course);
+            return "redirect:/course/add-course";
+        }catch (DateTimeParseException e){
+            ObjectError error = new ObjectError("fail","we can not add your course");
+            result.addError(error);
+            return "redirect:/course/add-course";
+        }
     }
 
     @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/delete-course/{id}")
-    public String deleteCourse(@PathVariable Long id){
+    public String deleteCourse(@PathVariable Long id) {
         courseService.delete(courseService.findByID(id).get());
         return "redirect:/course/add-course";
     }
 
     @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/add-teacher-to-course/{id}")
-    public String showAddTeacherToCoursePage(@PathVariable Long id, Model model){
+    public String showAddTeacherToCoursePage(@PathVariable Long id, Model model) {
         Course course = courseService.findByID(id).get();
-        model.addAttribute("course",context.getBean(CourseDTO.class,course));
-        model.addAttribute("newTeacher",context.getBean(TeacherDTO.class));
+        model.addAttribute("course", context.getBean(CourseDTO.class, course));
+        model.addAttribute("newTeacher", context.getBean(TeacherDTO.class));
         Function<Teacher, TeacherDTO> function = a -> {
-          TeacherDTO teacherDTO = context.getBean(TeacherDTO.class,a);
+            TeacherDTO teacherDTO = context.getBean(TeacherDTO.class, a);
             return teacherDTO;
         };
         List<TeacherDTO> teacherDTOList = teacherService.findTeachersAreEnabled().stream().map(function).collect(Collectors.toList());
-        model.addAttribute("teachers",teacherDTOList);
+        model.addAttribute("teachers", teacherDTOList);
         return "User/Admin/add-teacher-to-course";
     }
 
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/add-teacher-to-course/{id}")
-    public String AddTeacherToCoursePage(@ModelAttribute("newTeacher") TeacherDTO teacherDTO , @PathVariable Long id)  {
+    public String AddTeacherToCoursePage(@ModelAttribute("newTeacher") TeacherDTO teacherDTO, @PathVariable Long id) {
 
-        courseService.saveCourse(teacherDTO,id);
+        courseService.saveCourse(teacherDTO, id);
         return "redirect:/course/add-course";
     }
 
     @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/add-students-to-course/{id}")
-    public String showAddStudentsToCoursePage(@PathVariable Long id, Model model){
+    public String showAddStudentsToCoursePage(@PathVariable Long id, Model model) {
         Course course = courseService.findByID(id).get();
-        model.addAttribute("course",context.getBean(CourseDTO.class,course));
-        model.addAttribute("newStudent",context.getBean(StudentDTO.class));
+        model.addAttribute("course", context.getBean(CourseDTO.class, course));
+        model.addAttribute("newStudent", context.getBean(StudentDTO.class));
         Function<Student, StudentDTO> function = a -> {
-            StudentDTO studentDTO = context.getBean(StudentDTO.class,a);
+            StudentDTO studentDTO = context.getBean(StudentDTO.class, a);
             return studentDTO;
         };
         List<StudentDTO> studentDTOList1 = studentService.findStudentsAreEnable().stream().map(function).collect(Collectors.toList());
-        model.addAttribute("students",studentDTOList1);
+        model.addAttribute("students", studentDTOList1);
         List<StudentDTO> studentDTOList2 = studentService.findStudentsOfCourse(course).stream().map(function).collect(Collectors.toList());
-        model.addAttribute("studentsOfCourse",studentDTOList2);
+        model.addAttribute("studentsOfCourse", studentDTOList2);
         return "User/Admin/add-students-to-course";
     }
 
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/add-students-to-course/{id}")
-    public String addStudentsToCoursePage(@ModelAttribute("newStudent") StudentDTO studentDTO,@PathVariable Long id,BindingResult result){
-        studentService.saveStudent(studentDTO,id);
-        return "redirect:/course/add-students-to-course/"+id;
+    public String addStudentsToCoursePage(@ModelAttribute("newStudent") StudentDTO studentDTO, @PathVariable Long id, BindingResult result) {
+        studentService.saveStudent(studentDTO, id);
+        return "redirect:/course/add-students-to-course/" + id;
     }
 }
