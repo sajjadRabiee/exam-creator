e51366c1512d587e72e6c0c109f35c22f42bef93
Sajjad <Rabiee.614@gmail.com>
2021-01-19 14:36:11 +0330

    add exam dto for transfer object between back and front

--- src/main/java/com/examcreator/finalproject/entities/DTOEntities/ExamDTO.java
+++ src/main/java/com/examcreator/finalproject/entities/DTOEntities/ExamDTO.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Component;
 @Component
 @Scope("prototype")
 public class ExamDTO {
+    private Long id;
     private String title;
+    private String typeOfExam;
     private String description;
     private String date;
     private String startTime;
@@ -18,11 +20,22 @@ public class ExamDTO {
     }
 
     public ExamDTO(Exam exam) {
+        this.id = exam.getId();
         this.title = exam.getTitle();
         this.description = exam.getDescription();
         this.date = exam.getDate().toString();
         this.startTime = exam.getStartTime().toString();
         this.endTime = exam.getEndTime().toString();
+        this.typeOfExam = exam.getTypeOfExam().name();
+        System.out.println(this.typeOfExam);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTitle() {
@@ -31,6 +44,14 @@ public class ExamDTO {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getTypeOfExam() {
+        return typeOfExam;
+    }
+
+    public void setTypeOfExam(String typeOfExam) {
+        this.typeOfExam = typeOfExam;
     }
 
     public String getDescription() {
