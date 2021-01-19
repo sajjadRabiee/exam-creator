35dccee51f4574777ec8865c7bd68286fbe816c3
Sajjad <Rabiee.614@gmail.com>
2021-01-19 14:34:06 +0330

    complete main controller

--- src/main/java/com/examcreator/finalproject/controller/MainController.java
+++ src/main/java/com/examcreator/finalproject/controller/MainController.java
@@ -1,25 +1,18 @@
 package com.examcreator.finalproject.controller;
 
-import com.examcreator.finalproject.entities.classEntities.Admin;
-import com.examcreator.finalproject.entities.classEntities.Teacher;
-import com.examcreator.finalproject.entities.classEntities.User;
-import com.examcreator.finalproject.service.UserServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
 
     @GetMapping("/index")
-    public String showMainPage(){
+    public String showMainPage() {
         return "index";
     }
 
     @GetMapping("/login")
-    public String showLoginPage(){ return "login";}
+    public String showLoginPage() {
+        return "login";
+    }
 }
